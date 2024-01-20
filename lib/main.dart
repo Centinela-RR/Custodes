74023f814cb46df7d0c1e144c2646ab384d42bed
2021030063 <152040336+2021030063@users.noreply.github.com>
2024-01-20 01:49:28 -0600

    Reading db done
    - No auth to read tho
    - Modified the debug screen so that:
      - When button is pressed, info is fetched and echoed in the console, and text label changes
      - When pressed again, text label goes back to original state
    - Info fetched is stored, but never used; straight to print
    Idk how to use it yet

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:custodes/debug.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
@@ -9,7 +10,7 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const MyApp());
+  runApp(const DebugApp()); //MyApp() is default: the main app
 }
 
 class MyApp extends StatelessWidget {
@@ -21,21 +22,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Custodes',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -46,10 +32,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
   // This class is the configuration for the state. It holds the values (in this
   // case the title) provided by the parent (in this case the App widget) and
