42df32c16d27a5b12589ef39d4d4ab1def8d5602
Yoelito <129787922+Yoel1515@users.noreply.github.com>
2024-01-22 22:00:30 -0600

    Mas cosas para permisos
    mucha wea

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,21 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:device_info_plus/device_info_plus.dart';
+import 'dart:io';
+import 'package:permission_handler_platform_interface/permission_handler_platform_interface.dart';
 
+export 'package:permission_handler_platform_interface/permission_handler_platform_interface.dart'
+    show
+    Permission,
+    PermissionStatus,
+    PermissionStatusGetters,
+    PermissionWithService,
+    FuturePermissionStatusGetters,
+    ServiceStatus,
+    ServiceStatusGetters,
+    FutureServiceStatusGetters;
+
+PermissionHandlerPlatform get _handler => PermissionHandlerPlatform.instance;
 void main() async {
   // Ensure that Firebase is initialized before runApp() is called
   WidgetsFlutterBinding.ensureInitialized();
