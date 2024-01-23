21db3f92ae83ba751a63a8df679eaa5f8c7edf25
Yoelito <129787922+Yoel1515@users.noreply.github.com>
2024-01-22 19:53:17 -0600

    improtaciones
    lol

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:custodes/vista/debug.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 
 void main() async {
   // Ensure that Firebase is initialized before runApp() is called
