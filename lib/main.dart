ec30e5146a4c9ca9d432a1f958b26baf833b86c7
Yoelito <129787922+Yoel1515@users.noreply.github.com>
2024-01-28 16:14:57 -0600

    Movidas para el splash-screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,12 @@
 import 'package:custodes/vista/debug.dart';
+import 'package:custodes/vista/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'dart:io';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:baseflow_plugin_template/baseflow_plugin_template.dart';
-import 'package:permission_handler_platform_interface/permission_handler_platform_interface.dart';
-
-export 'package:permission_handler_platform_interface/permission_handler_platform_interface.dart'
-    show
-    Permission,
-    PermissionStatus,
-    PermissionStatusGetters,
-    PermissionWithService,
-    FuturePermissionStatusGetters,
-    ServiceStatus,
-    ServiceStatusGetters,
-    FutureServiceStatusGetters;
-
-PermissionHandlerPlatform get _handler => PermissionHandlerPlatform.instance;
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -28,153 +15,18 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   // Run the app
-  runApp(DebugApp());
+  runApp(const MyApp());
 }
-///Defines the main theme color
-final MaterialColor themeMaterialColor =
-BaseflowPluginExample.createMaterialColor(
-    const Color.fromRGBO(48, 49, 60, 1));
-
-/// A Flutter application demonstrating the functionality of this plugin
-class PermissionHandlerWidget extends StatefulWidget {
-  /// Create a page containing the functionality of this plugin
-  static ExamplePage createPage() {
-    return ExamplePage(
-        Icons.location_on, (context) => PermissionHandlerWidget());
-  }
-
-  @override
-  _PermissionHandlerWidgetState createState() =>
-      _PermissionHandlerWidgetState();
-}
-
-class _PermissionHandlerWidgetState extends State<PermissionHandlerWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ListView(
-          children: Permission.values
-              .where((permission) {
-            if (Platform.isIOS) {
-              return permission != Permission.unknown &&
-                  permission != Permission.phone &&
-                  permission != Permission.sms &&
-                  permission != Permission.ignoreBatteryOptimizations &&
-                  permission != Permission.accessMediaLocation &&
-                  permission != Permission.activityRecognition &&
-                  permission != Permission.manageExternalStorage &&
-                  permission != Permission.systemAlertWindow &&
-                  permission != Permission.requestInstallPackages &&
-                  permission != Permission.accessNotificationPolicy &&
-                  permission != Permission.bluetoothScan &&
-                  permission != Permission.bluetoothAdvertise &&
-                  permission != Permission.bluetoothConnect &&
-                  permission != Permission.nearbyWifiDevices &&
-                  permission != Permission.videos &&
-                  permission != Permission.audio &&
-                  permission != Permission.scheduleExactAlarm &&
-                  permission != Permission.sensorsAlways;
-            } else {
-              return permission != Permission.unknown &&
-                  permission != Permission.mediaLibrary &&
-                  permission != Permission.photosAddOnly &&
-                  permission != Permission.reminders &&
-                  permission != Permission.bluetooth &&
-                  permission != Permission.appTrackingTransparency &&
-                  permission != Permission.criticalAlerts &&
-                  permission != Permission.assistant;
-            }
-          })
-              .map((permission) => PermissionWidget(permission))
-              .toList()),
-    );
-  }
-}
-
-/// Permission widget containing information about the passed [Permission]
-class PermissionWidget extends StatefulWidget {
-  /// Constructs a [PermissionWidget] for the supplied [Permission]
-  const PermissionWidget(this._permission);
-
-  final Permission _permission;
-
-  @override
-  _PermissionState createState() => _PermissionState(_permission);
-}
-
-class _PermissionState extends State<PermissionWidget> {
-  _PermissionState(this._permission);
-
-  final Permission _permission;
-  PermissionStatus _permissionStatus = PermissionStatus.denied;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _listenForPermissionStatus();
-  }
-
-  void _listenForPermissionStatus() async {
-    final status = await _permission.status;
-    setState(() => _permissionStatus = status);
-  }
-
-  Color getPermissionColor() {
-    switch (_permissionStatus) {
-      case PermissionStatus.denied:
-        return Colors.red;
-      case PermissionStatus.granted:
-        return Colors.green;
-      case PermissionStatus.limited:
-        return Colors.orange;
-      default:
-        return Colors.grey;
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+    @override
+  Widget build(BuildContext context){
+      return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashScreen(),
+      );
     }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        _permission.toString(),
-        style: Theme.of(context).textTheme.bodyLarge,
-      ),
-      subtitle: Text(
-        _permissionStatus.toString(),
-        style: TextStyle(color: getPermissionColor()),
-      ),
-      trailing: (_permission is PermissionWithService)
-          ? IconButton(
-          icon: const Icon(
-            Icons.info,
-            color: Colors.white,
-          ),
-          onPressed: () {
-            checkServiceStatus(
-                context, _permission as PermissionWithService);
-          })
-          : null,
-      onTap: () {
-        requestPermission(_permission);
-      },
-    );
-  }
-
-  void checkServiceStatus(
-      BuildContext context, PermissionWithService permission) async {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text((await permission.serviceStatus).toString()),
-    ));
-  }
-
-  Future<void> requestPermission(Permission permission) async {
-    final status = await permission.request();
-
-    setState(() {
-      print(status);
-      _permissionStatus = status;
-      print(_permissionStatus);
-    });
-  }
 }
+
+
+
