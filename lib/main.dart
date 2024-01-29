ad4fa20053d2c78b7ffb88feb5d1a346543c998f
2021030063 <152040336+2021030063@users.noreply.github.com>
2024-01-28 19:02:52 -0600

    wtf did you do
    - Fix, now back, up and running
    - Remove unused packages, again

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:custodes/vista/debug.dart';
-import 'package:custodes/vista/splash_screen.dart';
+//import 'package:custodes/vista/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'modelo/firebase_options.dart';
 import 'package:flutter/material.dart';
-import 'package:device_info_plus/device_info_plus.dart';
-import 'dart:io';
-import 'package:baseflow_plugin_template/baseflow_plugin_template.dart';
-import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+//import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -15,7 +14,7 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   // Run the app
-  runApp(const MyApp());
+  runApp(const MaterialApp(home: DebugApp()));
 }
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
@@ -23,10 +22,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
       return const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: SplashScreen(),
+        home: DebugApp(),//SplashScreen aún no tiene un archivo
       );
     }
 }
-
-
-
--- lib/vista/debug.dart
+++ lib/vista/debug.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 import '../modelo/db.dart';
 
 class DebugApp extends StatefulWidget {
@@ -9,38 +11,188 @@ class DebugApp extends StatefulWidget {
 }
 
 class DebugAppState extends State<DebugApp> {
-  String buttonText = 'Sugma';
-  // Create new instance of FirebaseConnection called fb
+  late String widgetTitle, uniqId;
+  String buttonTitle = '';
   FirebaseConnection fb = FirebaseConnection();
+  late Timer timer;
+
+  @override
+  void initState() {
+    super.initState();
+    widgetTitle = ''; // Initial value
+    uniqId = ''; // Initial value
+    startTimer();
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(const Duration(milliseconds: 438), (timer) {
+      setState(() {
+        // Change the value of widgetTitle here
+        widgetTitle = widgetTitle == "Fuck" ? "This" : "Fuck"; 
+        uniqId = fb.generateLocalIdentifier();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    timer.cancel(); // Cancel the timer to avoid memory leaks
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Debug App',
+      title: 'Custodes - Debug App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text(buttonText),
+          title: Text(widgetTitle),
         ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () async {
-              // Your Firebase connection test logic here:
-              // When the button is pressed, call the function fetchElement
-              //fb.fetchElement();
-              // For a demo on playing with Flutter, 
-              // we'll change the button text
-              setState(() {
-                buttonText = fb.generateLocalIdentifier();
-                /*buttonText = buttonText == 'Sugma'
-                    ? 'BALLS'
-                    : 'Sugma';*/
-                
-              });
-            },
-            child: Text(buttonText),
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SpinningCylinder(), // This for a spinning bean
+            const SizedBox(height: 20), // Add some spacing
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: 
+              // FIXME: wtf why does this not show up in mobile?
+              <Widget>[
+                //const SizedBox(width: 20.0, height: 100.0),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Custodia tu',
+                    style: TextStyle(fontSize: 15.0),
+                  ),
+                ),
+                const SizedBox(width: 5.0, height: 10.0),
+                SizedBox(
+                  // TODO: If width too big, breaks mobile
+                  // JK IT DOESNT SHOW AT ALL FUCK WHY TF DOES IT SHOW IN WEB BUT NOT IN IOS DUD WTFFF
+                  // Se ve bonito en web pero en iOS no sale at all wtf
+                  width: 90,
+                  child: DefaultTextStyle(
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                      fontSize: 15.0,
+                      fontFamily: 'Horizon',
+                    ),
+                    child: AnimatedTextKit(
+                      animatedTexts: [
+                        RotateAnimatedText('Seguridad', alignment: Alignment.centerLeft, transitionHeight: 20),
+                        RotateAnimatedText('Paz', alignment: Alignment.centerLeft, transitionHeight: 20),
+                        RotateAnimatedText('Tranquilidad', alignment: Alignment.centerLeft, transitionHeight: 20),
+                      ],
+                      onTap: () {
+                        print("Tap Event");
+                      },
+                      /*onFinished: () {
+                        //FIXME: FUCKING MAKE THIS SHOW LAST AND NOT LEAVE FFS
+                        RotateAnimatedText('aaa', alignment: Alignment.center);
+                      },*/
+                      //isRepeatingAnimation: true,
+                      //totalRepeatCount: 1,
+                      repeatForever: true,
+                      pause: const Duration(milliseconds: 50),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Text("Unique id: $uniqId"), // Display uniqId directly as Text widget
+            const SizedBox(height: 20), // Add some spacing
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      buttonTitle = "Botón 1";
+                      uniqId = fb.generateLocalIdentifier();
+                    });
+                    _showAlertDialog(context, buttonTitle, uniqId);
+                  },
+                  child: const Text("Botón 1"),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      buttonTitle = "Botón 2";
+                      uniqId = fb.generateLocalIdentifier();
+                    });
+                    _showAlertDialog(context, buttonTitle, uniqId);
+                  },
+                  child: const Text("Botón 2"),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
+
+  void _showAlertDialog(BuildContext context, String title, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(buttonTitle),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
 
+
+class SpinningCylinder extends StatefulWidget {
+  const SpinningCylinder({super.key});
+
+  @override
+  SpinningCylinderState createState() => SpinningCylinderState();
+}
+
+class SpinningCylinderState extends State<SpinningCylinder> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 438*4),
+    )..repeat();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RotationTransition(
+      turns: _controller,
+      child: Container(
+        width: 100,
+        height: 200,
+        decoration: BoxDecoration(
+          color: Colors.blue, // You can adjust the color as needed
+          borderRadius: BorderRadius.circular(50),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
