61de4b147d389418d3419a1bba85dc79f27cdf13
Yoelito <129787922+Yoel1515@users.noreply.github.com>
2024-02-10 14:30:10 -0600

    Autenticacion de inicio de sesion
    -Al cerrar y volver a abrir la aplicación, se verifica el estado de autenticación del usuario. Si el usuario ya ha iniciado sesión, se registra un mensaje indicando que se ha entrado a la página de inicio (InicioPage). En caso contrario, se registra un mensaje indicando que se ha entrado a la página de inicio de sesión (LoginPage).

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'modelo/firebase_options.dart';
 import 'package:flutter/material.dart';
+//import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,6 +14,7 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  
 
   const bool useEmulator = true;
 
--- lib/vista/inicio.dart
+++ lib/vista/inicio.dart
@@ -1,5 +1,7 @@
 import 'package:custodes/vista/login.dart';
+//import 'package:custodes/vista/prueba_inicio.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -14,7 +16,33 @@ class MainApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginPage(), // * Cambiar a la vista de inicio de sesión
+      home: const AuthCheck(), // * Verifica el estado de autenticación
+    );
+  }
+}
+
+class AuthCheck extends StatelessWidget {
+  const AuthCheck({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // Si estamos esperando una respuesta de autenticación,
+          // se puede mostrar un indicador de carga o algo similar
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasData) {
+          // Si hay un usuario autenticado, muestra la pantalla de inicio
+          print('Entró a InicioPage');
+          return const SizedBox(); // No se muestra ninguna pantalla
+        } else {
+          // Si no hay un usuario autenticado, muestra la pantalla de inicio de sesión
+          print('Entró a LoginPage');
+          return const LoginPage();
+        }
+      },
     );
   }
 }
